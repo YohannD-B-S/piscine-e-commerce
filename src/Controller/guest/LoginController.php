949a2a6a259c4f35ae21c22233247d9cfc7e4189
Yohann Dubuis <yohann.dubuis@lapiscine.pro>
2025-05-12 15:32:15 +0200

    info admin connected

--- src/Controller/guest/LoginController.php
+++ src/Controller/guest/LoginController.php
@@ -4,13 +4,15 @@ namespace App\Controller\guest;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 
 {
     #[Route('/login', name: 'login')]
-    public function displayLogin()
+    public function displayLogin(AuthenticationUtils $authentication)
     {
+         
 
         return $this->render('guest/login.html.twig');
     }
