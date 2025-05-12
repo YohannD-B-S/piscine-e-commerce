ec7f67caeb95e582de39e6a93b89fb30c2232dc3
Yohann Dubuis <yohann.dubuis@lapiscine.pro>
2025-05-12 17:20:15 +0200

    admin creator dataBase Loader

--- src/Controller/admin/AdminUserController.php
+++ src/Controller/admin/AdminUserController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\admin;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -13,23 +14,24 @@ class AdminUserController extends AbstractController
 
 
     #[Route('/admin/create-admin', name: 'admin-create-admin')]
-    public function displayCreateUser(Request $request, UserPasswordHasherInterface $userPasswordHasher)
+    public function displayCreateUser(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager)
     {
+if ($request->isMethod('POST')) { // Vérifie si la requête est de type POST
+            $email = $request->request->get('email'); // Récupère l'email depuis la requête
+            $password = $request->request->get(key: 'password'); // Récupère le mot de passe depuis la requête
+            
+            $user = new User(); // Crée une nouvelle instance de l'utilisateur
+            
+            $passwordHashed = $userPasswordHasher->hashPassword($user, $password); // Hashage du mot de passe pour la sécurité
+            
+            $user->createAdmin($email, $passwordHashed); // Crée un administrateur avec l'email et le mot de passe sécurisé
+            
+            $entityManager->persist($user); // Prépare l’utilisateur pour l’enregistrement en base de données
+            $entityManager->flush(); // Sauvegarde les modifications dans la base de données
+            
+            $this->addFlash('success','Admin créé'); // Ajoute un message flash pour informer que l’admin a été créé
+        } // Fin du bloc conditionnel
 
-        if ($request->isMethod('POST')) {
-
-            $email = $request->request->get('email');
-            $password = $request->request->get(key: 'password');
-
-            $user = new User();
-
-            $passwordHashed = $userPasswordHasher->hashPassword($user, $password);
-
-            dump($email);
-            dump($passwordHashed);
-            die;
-        }
-
-        return $this->render('/admin/user/create-user.html.twig');
-    }
-}
+        return $this->render('/admin/user/create-user.html.twig'); // Affiche la page de création d’utilisateur
+    } // Fin de la méthode
+} // Fin de la classe
