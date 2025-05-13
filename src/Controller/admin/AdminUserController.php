0f47c2471e42a7791cf1b0703df6f16083cf09c1
Yohann Dubuis <yohann.dubuis@lapiscine.pro>
2025-05-13 10:15:06 +0200

    gestion des erreurs

--- src/Controller/admin/AdminUserController.php
+++ src/Controller/admin/AdminUserController.php
@@ -4,6 +4,7 @@ namespace App\Controller\admin;
 
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -26,10 +27,23 @@ if ($request->isMethod('POST')) { // Vérifie si la requête est de type POST
             
             $user->createAdmin($email, $passwordHashed); // Crée un administrateur avec l'email et le mot de passe sécurisé
             
-            $entityManager->persist($user); // Prépare l’utilisateur pour l’enregistrement en base de données
-            $entityManager->flush(); // Sauvegarde les modifications dans la base de données
+            try{
+                $entityManager->persist($user); // Prépare l’utilisateur pour l’enregistrement en base de données
+                $entityManager->flush(); // Sauvegarde les modifications dans la base de données
+                $this->addFlash('success','Admin créé'); // Ajoute un message flash pour informer que l’admin a été créé
+
+            }catch (Exception $exception){
+                $this->addFlash('error', 'Impossible de créée l\'admin');
+                
+                if ($exception->getcode()===1062){
+                    $this->addFlash ('error', 'Email déjà utilisé.');
+                }
+            }
+
             
-            $this->addFlash('success','Admin créé'); // Ajoute un message flash pour informer que l’admin a été créé
+            
+            
+            
         } // Fin du bloc conditionnel
 
         return $this->render('/admin/user/create-user.html.twig'); // Affiche la page de création d’utilisateur
